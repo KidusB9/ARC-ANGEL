f9d270c199b15c149a7cbe4b2c0efd2f36a506db
Kidus Berhanu <107410165+Kidus-berhanu@users.noreply.github.com>
2023-05-18 14:46:46 -0500

    Update package.js

--- package.js
+++ package.js
@@ -7,13 +7,13 @@
     "start": "node app.js"
   },
   "dependencies": {
-    "@google-cloud/vision": "^2.3.0",
-    "@google-cloud/language": "^4.3.0",
-    "axios": "^0.21.1",
+    "@google-cloud/vision": "^3.1.3",
+    "@google-cloud/language": "^5.2.2",
+    "axios": "^1.4.0",
     "assemblyai": "^1.0.0",
     "openai": "^3.2.1",
     "opencv4nodejs": "^9.2.0",
-    "dotenv": "^10.0.0"
+    "dotenv": "^16.0.3"
     "express": "^4.17.1"
   },
   "engines": {
