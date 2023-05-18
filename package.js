a0ac7787191d06ea40960d79514cbfab59f4b0e6
Kidus Berhanu <107410165+Kidus-berhanu@users.noreply.github.com>
2023-05-18 01:58:21 -0500

    Create package.js

---
+++ package.js
@@ -0,0 +1,21 @@
+{
+  "name": "my-app",
+  "version": "1.0.0",
+  "description": "An app that integrates multiple AI services",
+  "main": "server.js",
+  "scripts": {
+    "start": "node app.js"
+  },
+  "dependencies": {
+    "@google-cloud/vision": "^2.3.0",
+    "@google-cloud/language": "^4.3.0",
+    "axios": "^0.21.1",
+    "assemblyai": "^1.0.0",
+    "openai": "^3.2.1",
+    "opencv4nodejs": "^9.2.0",
+    "dotenv": "^10.0.0"
+  },
+  "engines": {
+    "node": "14.x"
+  }
+}
