6fce3ee1bacbd35d6649d8c0c3fb39a487609668
Kidus Berhanu <107410165+Kidus-berhanu@users.noreply.github.com>
2023-05-25 21:18:22 -0500

    Update Prototype.py

--- Prototype.py
+++ Prototype.py
@@ -1,60 +1,131 @@
-# Import necessary libraries
+import axios
 from google.cloud import vision
 from google.cloud import language_v1
-from assemblyai import AssemblyAi
-import openai
+import assemblyai
+from openai import OpenAIApi
+import cv2
+import numpy as np
+import io
 import os
+from flask import Flask
 
-# Set API keys
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "path_to_your_google_credentials.json"
-openai.api_key = 'your-openai-api-key'
-assemblyai_api_key = 'your-assembly-ai-api-key'
-
-# Initialize API clients
 vision_client = vision.ImageAnnotatorClient()
 language_client = language_v1.LanguageServiceClient()
-assemblyai_client = AssemblyAi(assemblyai_api_key)
+openai = OpenAIApi()
 
 def handle_request(user_speech):
-    # Transcribe speech to text using AssemblyAI
-    text = assemblyai_client.transcribe(user_speech)
-    
-    # Determine user's intent from the transcribed text
-    intent = detect_intent(text)
+# Transcribe speech to text using AssemblyAI
+text = transcribe_speech(user_speech)
+# Determine user's intent from the transcribed text
+intent = detect_intent(text)
 
-    if intent == 'objectDetection':
-        # If intent is object detection, capture image and process it
-        image = get_image_from_user()
-        response = vision_client.object_localization(image=image)
-        return process_object_detection_result(response)
-    
-    elif intent == 'sentimentAnalysis':
-        # If intent is sentiment analysis, analyze sentiment of text
-        document = language_v1.Document(content=text, type_=language_v1.Document.Type.PLAIN_TEXT)
-        sentiment = language_client.analyze_sentiment(document=document).document_sentiment
-        return process_sentiment_analysis_result(sentiment)
-
-    elif intent == 'generateText':
-        # If intent is text generation, generate text using OpenAI API
-        response = openai.Completion.create(engine="text-davinci-003", prompt=text, max_tokens=100)
-        return response.choices[0].text.strip()
-
-    else:
-        # If intent could not be determined, return a generic response
-        return 'Sorry, I did not understand your request.'
+if intent == 'objectDetection':
+    # If intent is object detection, capture image and process it
+    image = get_image_from_user()
+    result = vision_client.object_localization(image=image)
+    return process_object_detection_result(result)
+elif intent == 'sentimentAnalysis':
+    # If intent is sentiment analysis, analyze sentiment of text
+    document = language_v1.Document(content=text, type_=language_v1.Document.Type.PLAIN_TEXT)
+    result = language_client.analyze_sentiment(request={'document': document})
+    return process_sentiment_analysis_result(result)
+elif intent == 'generateText':
+    # If intent is text generation, generate text using OpenAI API
+    gpt_response = openai.Completion.create(engine='text-davinci-003', prompt=text, max_tokens=200)
+    return gpt_response['choices'][0]['text']
+else:
+    # If intent could not be determined, return a generic response
+    return 'Sorry, I did not understand your request.'
+def transcribe_speech(user_speech):
+response = axios.post('https://api.assemblyai.com/v2/transcript', json={'audio_url': user_speech},
+headers={'authorization': os.environ['ASSEMBLY_AI_API_KEY']})
+return response.json()['text']
 
 def detect_intent(text):
-    # This function should implement intent detection logic
-    pass
+# Lowercase the text for easier matching
+lower_text = text.lower()
+# Define some keywords for each intent
+object_detection_keywords = ['detect', 'object', 'identify', 'recognition']
+sentiment_analysis_keywords = ['sentiment', 'feeling', 'emotion', 'mood']
+generate_text_keywords = ['generate', 'text', 'write', 'create']
 
+# Check if the text contains any keywords for 'objectDetection'
+for keyword in object_detection_keywords:
+    if keyword in lower_text:
+        return 'objectDetection'
+
+# Check if the text contains any keywords for 'sentimentAnalysis'
+for keyword in sentiment_analysis_keywords:
+    if keyword in lower_text:
+        return 'sentimentAnalysis'
+
+# Check if the text contains any keywords for 'generateText'
+for keyword in generate_text_keywords:
+    if keyword in lower_text:
+        return 'generateText'
+
+# If no intent could be determined, return 'unknown'
+return 'unknown'
 def get_image_from_user():
-    # This function should implement image capture logic
-    pass
+video_cap = cv2.VideoCapture(0) # 0 for default camera
+frames = []
+while True:
+    # Read a new frame from video
+    ret, frame = video_cap.read()
 
+    if not ret:
+        break
+
+    # Resize the frame to reduce processing time
+    frame = cv2.resize(frame, (300, 300))
+
+    # Add frame to list of frames
+    frames.append(frame)
+
+video_cap.release()
+
+# Combine frames into a video buffer
+output = cv2.VideoWriter_fourcc(*'MJPG')
+out = cv2.VideoWriter('output.avi', output, 20.0, (300, 300))
+for frame in frames:
+    out.write(frame)
+out.release()
+
+# Read the video file into a buffer
+with open('output.avi', 'rb') as f:
+    content = f.read()
+
+return vision.Image(content=content)
 def process_object_detection_result(response):
-    # This function should process the result of object detection and return a meaningful response
-    pass
+detected_objects = response.localized_object_annotations
+message = "Detected objects are:\n"
+for i, obj in enumerate(detected_objects):
+    message += f"{i+1}. {obj.name} with confidence: {obj.score}\n"
 
-def process_sentiment_analysis_result(sentiment):
-    # This function should process the result of sentiment analysis and return a meaningful response
-    pass
+return message
+def process_sentiment_analysis_result(response):
+emotions = [
+{'range': [0.7, 1], 'label': 'very positive'},
+{'range': [0.25, 0.7], 'label': 'positive'},
+{'range': [-0.25, 0.25], 'label': 'neutral'},
+{'range': [-0.7, -0.25], 'label': 'negative'},
+{'range': [-1, -0.7], 'label': 'very negative'},
+]
+sentiment = response.document_sentiment.score
+for emotion in emotions:
+    if emotion['range'][0] <= sentiment <= emotion['range'][1]:
+        return f"The sentiment of the text is {emotion['label']}."
+app = Flask(name)
+
+@app.route('/api')
+def api():
+user_speech = 'https://url-to-audio-file' # URL to an audio file
+try:
+response = handle_request(user_speech)
+return response
+except Exception as e:
+print(e)
+return 'An error occurred.', 500
+
+if name == 'main':
+app.run()
