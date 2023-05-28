677e4a40c5065b684cb613ba0ba69389b92dd93d
Kidus Berhanu <107410165+Kidus-berhanu@users.noreply.github.com>
2023-05-27 22:27:52 -0500

    Update Prototype.py

--- Prototype.py
+++ Prototype.py
@@ -38,95 +38,103 @@ def handle_request(user_speech):
         return 'Sorry, I did not understand your request.'
 
 def transcribe_speech(user_speech):
-response = axios.post('https://api.assemblyai.com/v2/transcript', json={'audio_url': user_speech},
-headers={'authorization': os.environ['ASSEMBLY_AI_API_KEY']})
-return response.json()['text']
+    response = axios.post('https://api.assemblyai.com/v2/transcript', json={'audio_url': user_speech},
+                          headers={'authorization': os.environ['ASSEMBLY_AI_API_KEY']})
+    return response.json()['text']
 
 def detect_intent(text):
-# Lowercase the text for easier matching
-lower_text = text.lower()
-# Define some keywords for each intent
-object_detection_keywords = ['detect', 'object', 'identify', 'recognition']
-sentiment_analysis_keywords = ['sentiment', 'feeling', 'emotion', 'mood']
-generate_text_keywords = ['generate', 'text', 'write', 'create']
+    # Lowercase the text for easier matching
+    lower_text = text.lower()
+    # Define some keywords for each intent
+    object_detection_keywords = ['detect', 'object', 'identify', 'recognition']
+    sentiment_analysis_keywords = ['sentiment', 'feeling', 'emotion', 'mood']
+    generate_text_keywords = ['generate', 'text', 'write', 'create']
 
-# Check if the text contains any keywords for 'objectDetection'
-for keyword in object_detection_keywords:
-    if keyword in lower_text:
-        return 'objectDetection'
+    # Check if the text contains any keywords for 'objectDetection'
+    for keyword in object_detection_keywords:
+        if keyword in lower_text:
+            return 'objectDetection'
 
-# Check if the text contains any keywords for 'sentimentAnalysis'
-for keyword in sentiment_analysis_keywords:
-    if keyword in lower_text:
-        return 'sentimentAnalysis'
+    # Check if the text contains any keywords for 'sentimentAnalysis'
+    for keyword in sentiment_analysis_keywords:
+        if keyword in lower_text:
+            return 'sentimentAnalysis'
 
-# Check if the text contains any keywords for 'generateText'
-for keyword in generate_text_keywords:
-    if keyword in lower_text:
-        return 'generateText'
+    # Check if the text contains any keywords for 'generateText'
+    for keyword in generate_text_keywords:
+        if keyword in lower_text:
+            return 'generateText'
 
-# If no intent could be determined, return 'unknown'
-return 'unknown'
+    # If no intent could be determined, return 'unknown'
+    return 'unknown'
+
 def get_image_from_user():
-video_cap = cv2.VideoCapture(0) # 0 for default camera
-frames = []
-while True:
-    # Read a new frame from video
-    ret, frame = video_cap.read()
+    video_cap = cv2.VideoCapture(0) # 0 for default camera
+    frames = []
+    while True:
+        # Read a new frame from video
+        ret, frame = video_cap.read()
 
-    if not ret:
-        break
+        if not ret:
+            break
 
-    # Resize the frame to reduce processing time
-    frame = cv2.resize(frame, (300, 300))
+        # Resize the frame to reduce processing time
+        frame = cv2.resize(frame, (300, 300))
 
-    # Add frame to list of frames
-    frames.append(frame)
+        # Add frame to list of frames
+        frames.append(frame)
 
-video_cap.release()
+    video_cap.release()
+        # Combine frames into a video buffer
+        output = cv2.VideoWriter_fourcc(*'MJPG')
+        out = cv2.VideoWriter('output.avi', output, 20.0, (300, 300))
 
-# Combine frames into a video buffer
-output = cv2.VideoWriter_fourcc(*'MJPG')
-out = cv2.VideoWriter('output.avi', output, 20.0, (300, 300))
-for frame in frames:
-    out.write(frame)
-out.release()
+        for frame in frames:
+            out.write(frame)
 
-# Read the video file into a buffer
-with open('output.avi', 'rb') as f:
-    content = f.read()
+        out.release()
 
-return vision.Image(content=content)
+        # Read the video file into a buffer
+        with open('output.avi', 'rb') as f:
+            content = f.read()
+
+        return vision.Image(content=content)
+
 def process_object_detection_result(response):
-detected_objects = response.localized_object_annotations
-message = "Detected objects are:\n"
-for i, obj in enumerate(detected_objects):
-    message += f"{i+1}. {obj.name} with confidence: {obj.score}\n"
+    detected_objects = response.localized_object_annotations
+    message = "Detected objects are:\n"
+    for i, obj in enumerate(detected_objects):
+        message += f"{i+1}. {obj.name} with confidence: {obj.score}\n"
 
-return message
+    return message
+
 def process_sentiment_analysis_result(response):
-emotions = [
-{'range': [0.7, 1], 'label': 'very positive'},
-{'range': [0.25, 0.7], 'label': 'positive'},
-{'range': [-0.25, 0.25], 'label': 'neutral'},
-{'range': [-0.7, -0.25], 'label': 'negative'},
-{'range': [-1, -0.7], 'label': 'very negative'},
-]
-sentiment = response.document_sentiment.score
-for emotion in emotions:
-    if emotion['range'][0] <= sentiment <= emotion['range'][1]:
-        return f"The sentiment of the text is {emotion['label']}."
-app = Flask(name)
+    emotions = [
+        {'range': [0.7, 1], 'label': 'very positive'},
+        {'range': [0.25, 0.7], 'label': 'positive'},
+        {'range': [-0.25, 0.25], 'label': 'neutral'},
+        {'range': [-0.7, -0.25], 'label': 'negative'},
+        {'range': [-1, -0.7], 'label': 'very negative'},
+    ]
+    sentiment = response.document_sentiment.score
+    for emotion in emotions:
+        if emotion['range'][0] <= sentiment <= emotion['range'][1]:
+            return f"The sentiment of the text is {emotion['label']}."
+
+app = Flask(__name__)
 
 @app.route('/api')
 def api():
-user_speech = 'https://url-to-audio-file' # URL to an audio file
-try:
-response = handle_request(user_speech)
-return response
-except Exception as e:
-print(e)
-return 'An error occurred.', 500
+    user_speech = 'https://url-to-audio-file' # URL to an audio file
+    try:
+        response = handle_request(user_speech)
+        return response
+    except Exception as e:
+        print(e)
+        return 'An error occurred.', 500
 
-if name == 'main':
-app.run()
+if __name__ == '__main__':
+    app.run()
+
+
+
